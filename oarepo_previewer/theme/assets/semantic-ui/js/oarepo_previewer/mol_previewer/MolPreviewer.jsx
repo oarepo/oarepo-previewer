95b0f21642ee79b60f652f5a13b7fa4a4080172d
Marek Eibel <marek.eibel@cesnet.cz>
2026-08-20 09:35:09 +0200

    feat: try to add molstar as dependency

--- oarepo_previewer/theme/assets/semantic-ui/js/oarepo_previewer/mol_previewer/MolPreviewer.jsx
+++ oarepo_previewer/theme/assets/semantic-ui/js/oarepo_previewer/mol_previewer/MolPreviewer.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
+import { Viewer } from "molstar/build/viewer/molstar";
+import "molstar/build/viewer/molstar.css"; 
 
 export const MolPreviewer = ({ uri }) => {
-  
+  const viewerRef = useRef(null);
+  const pluginRef = useRef(null);
+
+  useEffect(() => {
+    const initMolstar = async () => {
+      if (!viewerRef.current) return;
+
+      pluginRef.current = await Viewer.create(viewerRef.current, {
+        layoutIsExpanded: false,
+        layoutShowControls: false,       
+        layoutShowRemoteState: false,
+        layoutShowSequence: false,      
+        layoutShowLog: false,           
+        layoutShowLeftPanel: false,     
+      });
+
+      if (uri) {
+        const extension = uri.split('.').pop().toLowerCase();
+
+        if (['mvsj', 'mvsx'].includes(extension)) {
+          const mvsFormat = extension === 'mvsx' ? 'mvsx' : 'mvsj';
+          await pluginRef.current.loadMvsFromUrl(uri, mvsFormat);
+        } 
+        else {
+          const structFormat = ['mmcif', 'cif'].includes(extension) ? 'mmcif' : 'pdb';
+          await pluginRef.current.loadStructureFromUrl(uri, structFormat, false);
+        }
+      }
+    };
+
+    initMolstar();
+
+    return () => {
+      if (pluginRef.current) {
+        pluginRef.current.plugin.dispose();
+      }
+    };
+  }, [uri]);
+
   return (
-    <button>
-      File URI: {uri}
-    </button>
+    <div style={{ position: "relative", width: "100%", height: "400px", border: "1px solid #ccc", borderRadius: "4px" }}>
+      <div 
+        ref={viewerRef} 
+        style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }} 
+      />
+    </div>
   );
 };
 
 MolPreviewer.propTypes = {
-  uri: PropTypes.string.isRequired
+  uri: PropTypes.string.isRequired,
 };
 
 export default MolPreviewer;
