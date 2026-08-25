4e7dc1d5a4b64db6a5703e8f18e0c99525934945
Marek Eibel <marek.eibel@cesnet.cz>
2026-08-25 11:36:37 +0200

    feat: implement error boundary for previewer

--- oarepo_previewer/theme/assets/semantic-ui/js/oarepo_previewer/molstar_previewer/index.js
+++ oarepo_previewer/theme/assets/semantic-ui/js/oarepo_previewer/molstar_previewer/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MolstarPreviewer from  "./MolstarPreviewer";
+import MolstarPreviewer from "./components/MolstarPreviewer";
+import PreviewerErrorBoundary from "./components/PreviewerErrorBoundary";
 
 const domContainer = document.getElementById("molstar-viewer");
 const paramsContainer = document.getElementById("molstar-viewer-params");
 
 if (domContainer) {
   ReactDOM.render(
-    <MolstarPreviewer uri={paramsContainer.getAttribute("data-uri")}/>,
-    domContainer
+    <PreviewerErrorBoundary>
+      <MolstarPreviewer uri={paramsContainer.getAttribute("data-uri")} />
+    </PreviewerErrorBoundary>,
+    domContainer,
   );
 }
